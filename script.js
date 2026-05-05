8dbf54b55a5340900f165785e22b5760dc4b972f
detallitos0426-oss <detallitos0426@gmail.com>
2026-05-05 15:19:30 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,57 @@
-// corazones flotando
-setInterval(() => {
-  const heart = document.createElement("div");
-  heart.innerHTML = "❤️";
-  heart.style.position = "fixed";
-  heart.style.left = Math.random() * 100 + "vw";
-  heart.style.top = "100vh";
-  heart.style.fontSize = "20px";
+const canvas = document.getElementById("galaxy");
+const ctx = canvas.getContext("2d");
 
-  document.body.appendChild(heart);
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
 
-  setTimeout(() => {
-    heart.remove();
-  }, 4000);
-}, 500);
+let stars = [];
+
+for (let i = 0; i < 200; i++) {
+  stars.push({
+    x: Math.random() * canvas.width,
+    y: Math.random() * canvas.height,
+    size: Math.random() * 2
+  });
+}
+
+function drawStars() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+  ctx.fillStyle = "white";
+  stars.forEach(star => {
+    ctx.beginPath();
+    ctx.arc(star.x, star.y, star.size, 0, Math.PI * 2);
+    ctx.fill();
+  });
+
+  requestAnimationFrame(drawStars);
+}
+
+drawStars();
+
+
+// 💖 FRASES EN DIFERENTES IDIOMAS
+const phrases = [
+  "Te amo",
+  "I love you",
+  "Je t’aime",
+  "Ti amo",
+  "Ich liebe dich",
+  "Eu te amo",
+  "愛してる",
+  "사랑해",
+  "Я тебя люблю",
+  "Te iubesc"
+];
+
+phrases.forEach((text, index) => {
+  const span = document.createElement("div");
+  span.classList.add("love");
+  span.innerText = text;
+
+  span.style.top = "50%";
+  span.style.left = "50%";
+  span.style.animationDelay = index + "s";
+
+  document.body.appendChild(span);
+});
