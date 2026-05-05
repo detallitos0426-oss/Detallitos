e0ee56aa7978224519ceb4dcafc4693606a00bf4
detallitos0426-oss <detallitos0426@gmail.com>
2026-05-04 19:44:48 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
+// corazones flotando
+setInterval(() => {
+  const heart = document.createElement("div");
+  heart.innerHTML = "❤️";
+  heart.style.position = "fixed";
+  heart.style.left = Math.random() * 100 + "vw";
+  heart.style.top = "100vh";
+  heart.style.fontSize = "20px";
 
+  document.body.appendChild(heart);
+
+  setTimeout(() => {
+    heart.remove();
+  }, 4000);
+}, 500);
