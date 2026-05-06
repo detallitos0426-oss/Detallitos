07b23000f6a6b17d076205c82bd0e4854726e0d3
detallitos0426-oss <detallitos0426@gmail.com>
2026-05-05 17:39:06 -0800

    Update script.js

--- script.js
+++ script.js
@@ -24,3 +24,53 @@ setInterval(() => {
   }, 5000);
 
 }, 300);
+
+/* ========================= */
+/* CAMBIAR SECCIONES */
+/* ========================= */
+function showSection(id) {
+
+  document.querySelectorAll("section").forEach(sec => {
+    sec.classList.remove("active");
+  });
+
+  document.getElementById(id).classList.add("active");
+}
+
+/* ========================= */
+/* GALAXIA */
+/* ========================= */
+const canvas = document.getElementById("galaxyCanvas");
+const ctx = canvas.getContext("2d");
+
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+// estrellas
+let stars = [];
+for (let i = 0; i < 200; i++) {
+  stars.push({
+    x: Math.random() * canvas.width,
+    y: Math.random() * canvas.height,
+    size: Math.random() * 2
+  });
+}
+
+// animación
+function draw() {
+  ctx.fillStyle = "black";
+  ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+  ctx.fillStyle = "white";
+
+  stars.forEach(s => {
+    ctx.beginPath();
+    ctx.arc(s.x, s.y, s.size, 0, Math.PI * 2);
+    ctx.fill();
+  });
+
+  requestAnimationFrame(draw);
+}
+
+// iniciar galaxia
+draw();
